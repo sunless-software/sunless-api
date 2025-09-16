114af09d88789382fb69d5635667ceba35484b0a
Deshens <matias.franco289@gmail.com>
2025-09-16 07:27:03 -0300

    checked /blogs endpoints and updated documentation

--- src/routes/blogs.ts
+++ src/routes/blogs.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import projectRoleMiddleware from "../middlewares/projectRoleMiddleware";
 import { GLOBAL_PERMISSIONS } from "../constants/globalPermissions";
 import { PROJECT_PERMISSIONS } from "../constants/projectPermissions";
@@ -20,43 +20,30 @@ blogsRouter.get(
 
 blogsRouter.post(
   "/",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const projectID = parseInt(req.params.projectID) || 0;
-    projectRoleMiddleware(
-      GLOBAL_PERMISSIONS.createBlogs,
-      PROJECT_PERMISSIONS.createBlogs,
-      projectID
-    )(req, res, next);
-  },
+  projectRoleMiddleware(
+    GLOBAL_PERMISSIONS.createBlogs,
+    PROJECT_PERMISSIONS.createBlogs
+  ),
   createBlogValidation,
   blogsController.createBlog
 );
 
 blogsRouter.patch(
   "/:blogID",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const projectID = parseInt(req.params.projectID) || 0;
-    req.body.projectID = projectID;
-    projectRoleMiddleware(
-      GLOBAL_PERMISSIONS.updateBlogs,
-      PROJECT_PERMISSIONS.updateBlogs,
-      projectID
-    )(req, res, next);
-  },
+  projectRoleMiddleware(
+    GLOBAL_PERMISSIONS.updateBlogs,
+    PROJECT_PERMISSIONS.updateBlogs
+  ),
   updateBlogValidation,
   blogsController.updateBlogs
 );
 
 blogsRouter.delete(
   "/:blogID",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const projectID = parseInt(req.params.projectID) || 0;
-    projectRoleMiddleware(
-      GLOBAL_PERMISSIONS.deleteBlogs,
-      PROJECT_PERMISSIONS.deleteBlogs,
-      projectID
-    )(req, res, next);
-  },
+  projectRoleMiddleware(
+    GLOBAL_PERMISSIONS.deleteBlogs,
+    PROJECT_PERMISSIONS.deleteBlogs
+  ),
   deleteBlogValidation,
   blogsController.deleteBlogs
 );
